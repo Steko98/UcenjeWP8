7eb4e89b74250a4f7b5cafd111bb21a4a6fe948e
Ivan Šteko <ivan.steko5@gmail.com>
2025-05-09 19:06:01 +0200

    komentiranje koda

--- trgovina.sql
+++ trgovina.sql
@@ -1,14 +1,15 @@
 ﻿--create database trgovina;
 --use trgovina;
 
-create table racuni(
-sifra int,
-broj varchar(10),
-kupac varchar(50)
-);
+--create table racuni(
+--sifra int,
+--broj varchar(10),
+--kupac varchar(50)
+--);
 
-create table proizvodi(
-sifra int,
-naziv varchar(50),
-cijena decimal(18,2)
-);
+--create table proizvodi(
+--sifra int,
+--naziv varchar(50),
+--cijena decimal(18,2)
+--);
+
