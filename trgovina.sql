d4da1fc3d9f5ca0866a3dbed748e7a7bc18cc876
Ivan Šteko <ivan.steko5@gmail.com>
2025-05-09 19:05:05 +0200

    kreiranje tablica racuni i proizvodi

---
+++ trgovina.sql
@@ -0,0 +1,14 @@
+﻿--create database trgovina;
+--use trgovina;
+
+create table racuni(
+sifra int,
+broj varchar(10),
+kupac varchar(50)
+);
+
+create table proizvodi(
+sifra int,
+naziv varchar(50),
+cijena decimal(18,2)
+);
