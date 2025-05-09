a61e02e93eae1c4c5edb23c1dc52328e86b53d57
Ivan Šteko <ivan.steko5@gmail.com>
2025-05-09 19:33:08 +0200

    kreitanje baze taxisluzba i tablice vozila

---
+++ taxisluzba.sql
@@ -0,0 +1,11 @@
+﻿--create database taxisluzba;
+--use taxisluzba;
+
+--create table vozila(
+--sifra int,
+--proizvodac varchar(50),
+--godina int,
+--registracija varchar(8),
+--aktivan bit
+--);
+
