4514cf9d2d6d84fb1f04666eef5c2ff285834c1e
Ivan Šteko <ivan.steko5@gmail.com>
2025-05-10 13:21:14 +0200

    kreiranje tablica izlozbe i djela

--- muzej.sql
+++ muzej.sql
@@ -4,5 +4,16 @@
 --create table izlozbe(
 --sifra int,
 --naziv varchar(50),
---aktivna bit
+--pocetak datetime,
+--kraj datetime,
+--aktivno bit
 --);
+
+--create table djela(
+--sifra int,
+--naziv varchar(50),
+--autorime varchar(50),
+--autorprezime varchar(50),
+--godina int
+--);
+
