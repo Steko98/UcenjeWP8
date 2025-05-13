e3aed2bbf28eb27f6b3b8bf10fad149880da5c72
Ivan Šteko <ivan.steko5@gmail.com>
2025-05-13 14:48:15 +0200

    kreirane tablice

--- urar.sql
+++ urar.sql
@@ -1,2 +1,39 @@
 --create database urar;
 --use urar;
+
+--create table urar(
+--sifra int,
+--ime varchar(30),
+--prezime varchar(30),
+--email varchar(100),
+--adresa varchar(100)
+--);
+
+--create table korisnici(
+--sifra int,
+--ime varchar(30),
+--prezime varchar(30),
+--email varchar(100)
+--);
+
+--create table satovi(
+--sifra int,
+--proizvodac varchar(100),
+--model varchar(50),
+--korisnik int
+--);
+
+--create table segrti(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50)
+--);
+
+--create table popravci(
+--sifra int,
+--usluga varchar(150),
+--cijena decimal(18,2),
+--pocetak datetime,
+--kraj datetime,
+--sat int
+--);
