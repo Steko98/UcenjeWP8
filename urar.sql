d24feaef93cb8106378a93c71dcb03fcea217324
Ivan Šteko <ivan.steko5@gmail.com>
2025-05-13 19:55:10 +0200

    dijagrami ssms trgovina i urar

--- urar.sql
+++ urar.sql
@@ -1,39 +1,46 @@
---create database urar;
---use urar;
+use master;
+go
+drop database if exists urar;
+go
+create database urar;
+go
+use urar;
 
---create table urar(
---sifra int,
---ime varchar(30),
---prezime varchar(30),
---email varchar(100),
---adresa varchar(100)
---);
+create table urari(
+sifra int not null primary key identity(1,1),
+ime varchar(30) not null,
+prezime varchar(30) not null,
+email varchar(100),
+adresa varchar(100),
+);
 
---create table korisnici(
---sifra int,
---ime varchar(30),
---prezime varchar(30),
---email varchar(100)
---);
+create table korisnici(
+sifra int not null primary key identity(1,1),
+ime varchar(30) not null,
+prezime varchar(30) not null,
+email varchar(100)
+);
 
---create table satovi(
---sifra int,
---proizvodac varchar(100),
---model varchar(50),
---korisnik int
---);
+create table satovi(
+sifra int not null primary key identity(1,1),
+proizvodac varchar(100),
+model varchar(50),
+korisnik int not null references korisnici(sifra)
+);
 
---create table segrti(
---sifra int,
---ime varchar(50),
---prezime varchar(50)
---);
+create table segrti(
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+urar int not null references urari(sifra)
+);
 
---create table popravci(
---sifra int,
---usluga varchar(150),
---cijena decimal(18,2),
---pocetak datetime,
---kraj datetime,
---sat int
---);
+create table popravci(
+sifra int not null primary key identity(1,1),
+usluga varchar(150) not null,
+cijena decimal(18,2) not null,
+pocetak datetime,
+kraj datetime,
+sat int not null references satovi(sifra),
+urar int not null references urari(sifra)
+);
