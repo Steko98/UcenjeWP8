05ec89c97c2ee31a9345ba5e5aca19184adb4be9
Ivan Šteko <ivan.steko5@gmail.com>
2025-05-13 15:28:16 +0200

    kreirane tablice

--- ordinacija.sql
+++ ordinacija.sql
@@ -1,2 +1,41 @@
 ﻿--create database ordinacija;
 --use ordinacija;
+
+--create table doktori(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--email varchar(100),
+--kontaktbroj varchar(20),
+--odjel varchar(100)
+--);
+
+--create table pacijenti(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--spol varchar(20),
+--kontakt varchar(20),
+--krvnagrupa varchar(10)
+--);
+
+--create table medicinskesestre(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--odjel varchar(100),
+--kontakt varchar(20)
+--);
+
+--create table lijecenja(
+--sifra int,
+--datum datetime,
+--pacijent int,
+--doktor int
+--);
+
+--create table pomoci(
+--sifra int,
+--medicinskasestra int,
+--lijecenje int
+--);
