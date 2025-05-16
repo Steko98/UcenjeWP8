6abac209456585b048e3460cd075d976b24d9a06
Ivan Šteko <ivan.steko5@gmail.com>
2025-05-16 19:56:50 +0200

    update i delete naredbe

---
+++ update_delete.sql
@@ -0,0 +1,23 @@
+﻿use edunovawp8;
+
+select * from smjerovi where sifra=2;
+
+update smjerovi
+set aktivan=1
+where sifra=2;
+
+update smjerovi set
+cijena=1000,
+datumpokretanja='2024-02-29'
+where sifra=3;
+
+update smjerovi set cijena=1000 where cijena is null;
+
+update smjerovi set cijena = cijena * 1.12;
+
+update smjerovi set cijena = cijena * 0.9;
+update smjerovi set cijena = cijena - 100;
+
+delete smjerovi where sifra=1;
+delete grupe where smjer=1;
+delete clanovi where grupa in (select sifra from grupe where smjer=1);
