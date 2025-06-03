cad016706740adf95931de74480911cc4e1cd6a2
Ivan Šteko <ivan.steko5@gmail.com>
2025-06-03 19:52:37 +0200

    UlazIzlaz

---
+++ BACKEND/Ucenje/E01UlazIzlaz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    class E01UlazIzlaz
+    {
+        public static void Izvedi()
+        {
+
+
+
+            Console.Write("Pozdrav svijetu");
+            
+            Console.WriteLine("Isti red");
+
+            Console.WriteLine("Osijek je \nSUPER");
+
+            Console.WriteLine("\t\tOvo je uvuceno");
+
+            Console.Write("Unesi svoje ime: ");
+
+            // vraijabla ime
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Unijeli ste {0}", ime);
+
+
+
+        }// kraj metode
+
+
+
+
+
+    }// kraj klase
+}// kraj imenskog prostora (namespace)
