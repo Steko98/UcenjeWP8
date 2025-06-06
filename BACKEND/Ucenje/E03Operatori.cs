1ca4d2123252d0475949058a07b072d4bb8f15c0
Ivan Šteko <ivan.steko5@gmail.com>
2025-06-06 19:51:13 +0200

    operatori

---
+++ BACKEND/Ucenje/E03Operatori.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    class E03Operatori
+    {
+        public static void Izvedi()
+        {
+            string grad = "Osijek";
+            string ime = "Edunova";
+
+            //Console.WriteLine(grad + " " + ime + " " + 5);
+            //Console.WriteLine("{0} {1} {2}", grad, ime, 5);
+
+            //Console.WriteLine(8 % 2);
+            //int b = 9 % 2;
+            //Console.WriteLine(b);
+
+            int i = 0;
+            //i = i + 1;
+            //i += 1;
+            i++; // i++, ++i, i--, --i
+            ++i;
+
+            int k = 1, j = 0;
+            k = ++k - j++;
+
+            Console.WriteLine(--k+j++);
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
