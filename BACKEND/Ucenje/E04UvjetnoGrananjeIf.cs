b8e257f53455ba2bcb84b55ca11ffc99a5728d1a
Ivan Šteko <ivan.steko5@gmail.com>
2025-06-06 19:51:28 +0200

    uvjetno grananje

---
+++ BACKEND/Ucenje/E04UvjetnoGrananjeIf.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    class E04UvjetnoGrananjeIf
+    {
+        public static void Izvedi()
+        {
+            Console.WriteLine("E04UvjetnoGrananjeIf");
+
+            int broj = 7;
+
+            bool uvjet = broj == 7;
+
+            if (uvjet)
+            {
+                Console.WriteLine("Super");
+            }
+
+            if (broj > 2)
+            {
+                Console.WriteLine("ok");
+            }
+            else if (broj == 1)
+            {
+                Console.WriteLine("Nije dobro");
+            }
+            else
+            {
+                Console.WriteLine("Nije ocjena");
+            }
+
+            int i = 5, j = 10;
+
+            if(i>10 & j> 2){
+                Console.WriteLine("Nece se ispisati");
+            }
+
+            if (i > 10 && j > 2)
+            {
+                Console.WriteLine("Nece se ispisati");
+            }
+
+            if(i==4 || j == 2)
+            {
+                Console.WriteLine("Jedno od dvoje");
+            }
+
+            if (!uvjet)
+            {
+                Console.WriteLine("Kada gore ne ispise, ovdje ce");
+            }
+
+
+
+            string grad = "Osijek";
+            if(uvjet && i > 10)
+            {
+                if(grad == "Vukovar")
+                {
+                    Console.WriteLine("Novi prvoligas");
+                }
+            }
+
+            if (grad == "Osijek")
+            {
+                Console.WriteLine("Super");
+            } else
+            {
+                Console.WriteLine("ok");
+            }
+
+            Console.WriteLine(grad=="Osijek" ? "Super" : "OK");
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ BACKEND/Ucenje/E05UvjetnoGrananjeSwitch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    class E05UvjetnoGrananjeSwitch
+    {
+        public static void Izvedi()
+        {
+            Console.WriteLine("E05");
+
+            Console.Write("Unesi ocjenu: ");
+            int broj = int.Parse(Console.ReadLine());
+
+            switch (broj)
+            {
+                case 1:
+                    Console.WriteLine("Nedovoljan");
+                    break;
+                case 2:
+                    Console.WriteLine("Dovoljan");
+                    break;
+                case 3:
+                    Console.WriteLine("Dobar");
+                    break;
+                case 4:
+                    Console.WriteLine("Vrlo dobar");
+                    break;
+                case 5:
+                    Console.WriteLine("Izvrstan");
+                    break;
+                case 6:
+                case 7:
+                    Console.WriteLine("I to bi mogle biti ocjene ali nisu");
+                    break;
+                default:
+                    Console.WriteLine("Nije ocjena");
+                    break;
+            }
+        }
+    }
+}
