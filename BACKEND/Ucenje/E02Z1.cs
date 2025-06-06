a6b12cf5e44901a3d12134f99c239e63ad5777bc
Ivan Šteko <ivan.steko5@gmail.com>
2025-06-06 19:51:05 +0200

    zadatak

---
+++ BACKEND/Ucenje/E02Z1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    class E02Z1
+    {
+        public static void Izvedi()
+        {
+            Console.Write("Unesi svoju placu: ");
+            double placa = double.Parse(Console.ReadLine());
+            Console.WriteLine(placa);
+        }
+    }
+}
