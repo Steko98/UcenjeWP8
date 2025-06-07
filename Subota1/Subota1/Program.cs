997143ce8a7bb6654d3920c2c980a0106f195ad5
Ivan Šteko <ivan.steko5@gmail.com>
2025-06-07 11:26:46 +0200

    Izracunaj godine vjezba

--- Subota1/Subota1/Program.cs
+++ Subota1/Subota1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Subota1;
 
 //V01OstatakApp.Izvedi();
-V02TrošakPuta.Izvedi();
+//V02TrošakPuta.Izvedi();
+V03IzracunajGodine.Izvedi();
---
+++ Subota1/Subota1/V03IzracunajGodine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Subota1
+{
+    internal class V03IzracunajGodine
+    {
+        public static void Izvedi()
+        {
+            Console.Write("Unesite svoju godinu rođenja: ");
+            int godinaRodenja = int.Parse(Console.ReadLine());
+            Console.WriteLine("Imate {0} godina", 2025-godinaRodenja);
+        }
+    }
+}
