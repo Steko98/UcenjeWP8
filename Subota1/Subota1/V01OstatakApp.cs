f69840d472e770f8f42f3c5e4320e43f37a48659
Ivan Šteko <ivan.steko5@gmail.com>
2025-06-07 11:06:56 +0200

    Modulo app

---
+++ Subota1/Subota1/V01OstatakApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Subota1
+{
+    internal class V01OstatakApp
+    {
+        public static void Izvedi()
+        {
+            Console.WriteLine("Operator modulo %");
+            Console.Write("Unesi prvi (cijeli) broj: ");
+            int prviBroj = int.Parse(Console.ReadLine());
+            Console.Write("Unesi drugi (cijeli) broj: ");
+            int drugiBroj = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ostatak je {0}", prviBroj%drugiBroj);
+
+        }
+    }
+}
